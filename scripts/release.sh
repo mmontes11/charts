690a2f69fc2cc95422db402a7afe11a8375701bf
Martín Montes <martin11lrx@gmail.com>
2022-02-19 21:16:38 +0100

    Pin helm cm-push version

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-helm plugin install https://github.com/chartmuseum/helm-push.git
+helm plugin install https://github.com/chartmuseum/helm-push.git --version=v0.10.2
 helm repo add mmontes https://charts.mmontes-dev.duckdns.org
 
 source ./scripts/common.sh
