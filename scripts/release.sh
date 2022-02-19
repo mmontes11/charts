c073a06ecfb3cc9bd4df9bc89dbcd585ae132f89
Martín Montes <martin11lrx@gmail.com>
2022-02-19 20:42:36 +0100

    Ping helm push plugin version to v0.9.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-helm plugin install https://github.com/chartmuseum/helm-push.git
+helm plugin install https://github.com/chartmuseum/helm-push.git --version=v0.9.0
 helm repo add mmontes https://charts.mmontes-dev.duckdns.org
 
 source ./scripts/common.sh
 update_deps
 
 for path in $(ls -d charts/*); do
-    name=$(basename "$path")
-    echo "📦 Releasing '${name}'..."
-    helm push "$path" mmontes
+  name=$(basename "$path")
+  echo "📦 Releasing '${name}'..."
+  helm push "$path" mmontes
 done
