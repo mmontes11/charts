04df270cb3825ad5478474d8607d9a654ebf3cbf
Martín Montes <martin11lrx@gmail.com>
2022-02-20 15:25:03 +0100

    Refactored namespaces and deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,28 +5,34 @@ set -e
 source ./scripts/common.sh
 update_deps
 
-echo "☸️  Loading manifests..."
-kubectl apply -f manifests/
+echo "☸️  Creating namespaces..."
+kubectl apply -f manifests/namespaces
+
+echo "☸️  Creating secrets..."
+kubectl apply -f manifests/secrets
 
 echo "🚀 Deploying monitoring..."
 helm upgrade monitoring charts/monitoring --install --wait --namespace monitoring
 
-charts=(
-  nfs-provisioner
-  traefik
-  duckdns
-  pi-hole
-  github-explorer
-  iot
-  crypto-trade
-  gotway
-  mmontes-charts
-  mmontes-media
-  xiaowen-media
-)
+echo "🚀 Deploying traefik..."
+helm upgrade traefik charts/traefik --install --namespace kube-system
 
-for chart in "${charts[@]}"; do
-  echo "🚀 Deploying '${chart}'..."
-  chart_path="charts/${chart}"
-  helm upgrade --install "$chart" "$chart_path"
-done
+echo "🚀 Deploying nfs provisioner..."
+helm upgrade nfs charts/nfs-provisioner --install --namespace kube-system
+
+echo "🚀 Deploying dns..."
+helm upgrade duckdns charts/duckdns --install --namespace dns
+helm upgrade pi-hole  charts/pi-hole --install --namespace dns
+
+echo "🚀 Deploying chartmuseum..."
+helm upgrade mmontes-charts charts/mmontes-charts --install --namespace chartmuseum
+
+echo "🚀 Deploying media..."
+helm upgrade mmontes-media  charts/mmontes-media --install --namespace media
+helm upgrade xiaowen-media charts/xiaowen-media --install --namespace media
+
+echo "🚀 Deploying iot..."
+helm upgrade iot charts/iot --install --namespace iot
+
+echo "🚀 Deploying github explorer..."
+helm upgrade github-explorer charts/github-explorer --install --namespace github-explorer
