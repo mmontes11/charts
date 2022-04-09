8b14c92a3533310ea99b6b40fa99573163b05ed2
Martín Montes <martin11lrx@gmail.com>
2022-04-09 14:20:52 +0200

    Release improvements

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,17 +2,16 @@
 
 set -e
 
+# TODO: update default when deploying to production
 HELM_REPO_URL="${HELM_REPO_URL:-https://charts.mmontes-dev-v2.duckdns.org}"
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 
-helm plugin install https://github.com/chartmuseum/helm-push.git --version=v0.10.2
 helm repo add mmontes $HELM_REPO_URL
-
 source $CURRENT_DIR/scripts/common.sh
 update_deps
 
-for path in $(ls -d charts/*); do
+for path in $(ls -d $CURRENT_DIR/charts/*); do
   name=$(basename "$path")
-  echo "📦 Releasing '${name}'..."
+  echo "📦 Releasing '$name'..."
   helm cm-push "$path" mmontes
 done
