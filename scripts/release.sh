a7b7b0b5e727f7d0755c03540754b259d9e3ac4f
Martín Montes <martin11lrx@gmail.com>
2022-04-09 14:31:02 +0200

    Simplified release process

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,12 +6,21 @@ set -e
 HELM_REPO_URL="${HELM_REPO_URL:-https://charts.mmontes-dev-v2.duckdns.org}"
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 
+echo "☸️  Updating repos..."
 helm repo add mmontes $HELM_REPO_URL
-source $CURRENT_DIR/scripts/common.sh
-update_deps
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add chartmuseum https://chartmuseum.github.io/charts
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo add liwenhe https://liwenhe1993.github.io/charts/
+helm repo add nats https://nats-io.github.io/k8s/helm/charts/
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+helm repo update
 
-for path in $(ls -d $CURRENT_DIR/charts/*); do
+for path in $(ls -d $CURRENT_DIR/../charts/*); do
   name=$(basename "$path")
   echo "📦 Releasing '$name'..."
+  helm dep update "$path"
   helm cm-push "$path" mmontes
 done
