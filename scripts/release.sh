2d20861dd20166e797376208c8a5900e578d6b2f
Martín Montes <martin11lrx@gmail.com>
2022-04-09 12:02:24 +0200

    Fixed relative path

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,11 +3,12 @@
 set -e
 
 HELM_REPO_URL="${HELM_REPO_URL:-https://charts.mmontes-dev-v2.duckdns.org}"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 
 helm plugin install https://github.com/chartmuseum/helm-push.git --version=v0.10.2
 helm repo add mmontes $HELM_REPO_URL
 
-source ./scripts/common.sh
+source $CURRENT_DIR/scripts/common.sh
 update_deps
 
 for path in $(ls -d charts/*); do
