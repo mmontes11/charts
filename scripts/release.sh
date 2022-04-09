3eacda3f24e392943366c597e1d3a16760144a6c
Martín Montes <martin11lrx@gmail.com>
2022-04-09 11:55:34 +0200

    Fixed default variable

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-HELM_REPO_URL ?= https://charts.mmontes-dev-v2.duckdns.org
+HELM_REPO_URL="${HELM_REPO_URL:-https://charts.mmontes-dev-v2.duckdns.org}"
 
 helm plugin install https://github.com/chartmuseum/helm-push.git --version=v0.10.2
 helm repo add mmontes $HELM_REPO_URL
