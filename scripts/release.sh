e6704c844e261d728ee1aa9fd92b26b1cbee469b
Martín Montes <martin11lrx@gmail.com>
2022-04-09 11:36:05 +0200

    Using mmontes-v2 repo

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@
 set -e
 
 helm plugin install https://github.com/chartmuseum/helm-push.git --version=v0.10.2
-helm repo add mmontes https://charts.mmontes-dev.duckdns.org
+helm repo add --insecure-skip-tls-verify mmontes-v2 https://charts.mmontes-dev-v2.duckdns.org
 
 source ./scripts/common.sh
 update_deps
@@ -11,5 +11,5 @@ update_deps
 for path in $(ls -d charts/*); do
   name=$(basename "$path")
   echo "📦 Releasing '${name}'..."
-  helm cm-push "$path" mmontes
+  helm cm-push "$path" mmontes-v2
 done
