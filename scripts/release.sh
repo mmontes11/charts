c9ce4f2216ec9560fca133c864dedcb38c39ba03
Martín Montes <martin11lrx@gmail.com>
2022-05-08 16:48:51 +0200

    Removed uneeded repos

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,14 +9,8 @@ echo "☸️  Starting helm chart release for '$HELM_REPO_URL' repository"
 
 echo "☸️  Updating repos..."
 helm repo add mmontes $HELM_REPO_URL
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo add chartmuseum https://chartmuseum.github.io/charts
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm repo add liwenhe https://liwenhe1993.github.io/charts/
 helm repo add nats https://nats-io.github.io/k8s/helm/charts/
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add traefik https://helm.traefik.io/traefik
-helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 helm repo update
 
 echo "☸️  Updating dependencies..."
